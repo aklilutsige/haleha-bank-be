52ac3122eaf73511c3c23b647078b94389379364
Aklilu Tsige <aklilutsige@gmail.com>
2022-05-20 00:15:31 +0200

    Added spring securtiy user and passwork and Rest configuration

--- src/main/java/com/haleha/halehabank/HalehaBankApplication.java
+++ src/main/java/com/haleha/halehabank/HalehaBankApplication.java
@@ -2,8 +2,10 @@ package com.haleha.halehabank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.haleha.controller")
 public class HalehaBankApplication {
 
     public static void main(String[] args) {
