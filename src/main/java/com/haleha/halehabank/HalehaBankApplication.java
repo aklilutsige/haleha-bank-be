9ee283ed0809cbe37d6f4db6b0d617182f1ee240
Aklilu Tsige <aklilutsige@gmail.com>
2022-05-19 23:29:45 +0200

    Init haleha-bank-app backend project

---
+++ src/main/java/com/haleha/halehabank/HalehaBankApplication.java
@@ -0,0 +1,13 @@
+package com.haleha.halehabank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HalehaBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HalehaBankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/haleha/halehabank/HalehaBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haleha.halehabank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HalehaBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
