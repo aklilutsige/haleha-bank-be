5e8d33127fa64abdc4d2af2a5587c0d82454bf65
Aklilu Tsige <aklilutsige@gmail.com>
2022-06-03 10:51:05 +0200

    Mofify general project stracture and update db drive class

--- src/main/java/com/haleha/HalehaBankApplication.java
+++ src/main/java/com/haleha/HalehaBankApplication.java
@@ -1,20 +1,16 @@
-package com.haleha.halehabank;
+package com.haleha;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ComponentScans;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+//@ComponentScans({
+//        @ComponentScan("com.haleha.controller"),
+//        @ComponentScan("com.haleha.config")
+//})
+//@EnableJpaRepositories("com.haleha.repository")
+//@EntityScan("com.haleha.model")
 
-//@SpringBootApplication(exclude = UserDetailsServiceAutoConfiguration.class)
 @SpringBootApplication
-@ComponentScans({
-        @ComponentScan("com.haleha.controller"),
-        @ComponentScan("com.haleha.config")
-})
-@EnableJpaRepositories("com.haleha.repository")
-@EntityScan("com.haleha.model")
+//@SpringBootApplication(exclude = UserDetailsServiceAutoConfiguration.class)
 //@EnableWebSecurity(debug = true)
 public class HalehaBankApplication {
 
